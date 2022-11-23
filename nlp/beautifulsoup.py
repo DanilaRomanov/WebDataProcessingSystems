24a62a118dc54eb0e7e95a8c3513b205c61f4fd2
fatimaaaaaaa <44090391+fatimaaaaaaa@users.noreply.github.com>
2022-11-23 18:58:35 +0100

    Update beautifulsoup.py

--- nlp/beautifulsoup.py
+++ nlp/beautifulsoup.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 
 
 def bs_time(html_doc):
-    soup = BeautifulSoup(html_doc)
+    soup = BeautifulSoup(html_doc, features="lxml")
     # print(soup.prettify())
     # return soup.prettify()
     return soup.get_text()
