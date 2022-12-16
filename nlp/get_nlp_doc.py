c3312e60c575d6aa977a333cc81a40a19a61a973
fatimaaaaaaa <44090391+fatimaaaaaaa@users.noreply.github.com>
2022-12-16 20:26:21 +0100

    OH i just had to update the language pack:==)

--- nlp/get_nlp_doc.py
+++ nlp/get_nlp_doc.py
@@ -11,30 +11,30 @@ def get_nlp_doc(url, nlp):
     # print('\n============= RAW TEXT =============\n')
     # print(stripped_text)
 
-    try:
-        for text in stripped_text[0:6]:
-            if(len(text)) < 2:
+    # try:
+    for text in stripped_text[0:6]:
+        if(len(text)) < 2:
+            continue
+
+        for char in text:
+            if char.isascii():
+                # char.encode(encoding='UTF-8').decode('ascii')
                 continue
+            else:
+                print('========== char:', char)
+                text = text.replace(char, '')
 
-            for char in text:
-                if char.isascii():
-                    # char.encode(encoding='UTF-8').decode('ascii')
-                    continue
-                else:
-                    print('========== char:', char)
-                    text = text.replace(char, '')
+        print('\n============= RAW TEXT =============\n')
+        print('length:', len(text))
+        text = str(text)
+        print('type:', type(text))
+        print('text:', text)
 
-            print('\n============= RAW TEXT =============\n')
-            print('length:', len(text))
-            text = str(text)
-            print('type:', type(text))
-            print('text:', text)
+        doc = nlp(str(text))
 
-            doc = nlp(str(text))
-
-            print('\n============= STRIPPED TEXT =============\n')
-            print(doc)
+        print('\n============= STRIPPED TEXT =============\n')
+        print(doc)
 
         # return doc
-    except:
-        print("Error:", sys.exc_info())
+    # except:
+    #     print("Error:", sys.exc_info())
