a6d357601611dad01839d68b8c743f748a36c497
DanilaRomanov <d.v.romanov@student.tudelft.nl>
2022-12-21 21:21:58 +0100

    Relation extraction in pipeline

--- corpus_processing/coref.py
+++ corpus_processing/coref.py
@@ -1,21 +1,16 @@
 import spacy
-
 nlp = spacy.load("en_coreference_web_trf")
-
 
 def getClusters(text):
     #Returns co-reference clusters from a text
     doc = nlp(text)
     return doc
 
-
 def coref_resolution(text):
     output = ""
     doc = getClusters(text)
-
     #Extract the clusters
     clusters = [val for key, val in doc.spans.items() if key.startswith("coref_cluster")]
-
     token_dict = {}
     for cluster in clusters:
         first = cluster[0] #The first mention in a cluster is usually the entity
@@ -24,7 +19,6 @@ def coref_resolution(text):
             token_dict[repeats[0].idx] = first.text
             for token in repeats[1:]:
                 #Catch repeats that are many words
-
                 token_dict[token.idx] = ""
     for token in doc:
         if token.idx in token_dict:
--- corpus_processing/relation_extraction.py
+++ corpus_processing/relation_extraction.py
@@ -1,14 +1,13 @@
 import spacy
 import pathlib
+import coref
 import numpy as np
 import pandas as pd
 from spacy.matcher import Matcher
+nlp = spacy.load("en_core_web_md")
 
-# nlp = spacy.load("en_core_web_md")
-nlp = spacy.load("en_core_web_trf")
-
-
-def _find_verbs(doc):
+def find_verbs(doc):
+    #Finds verb part of speech tags in a spacy doc
     matcher = Matcher(nlp.vocab)
     pattern = [[{"POS": "VERB"}]]
     matcher.add("Verbs", pattern)
@@ -19,43 +18,42 @@ def _find_verbs(doc):
     return verbs
 
 
-def _longest_span(spans):
-    if len(spans) == 0:
+
+def longest_span(spans):
+    #Finds the longest relation span in a table
+    if (len(spans) == 0):
         return None
     sorted_spans = sorted(spans, key=lambda s: len(s), reverse=True)
     return sorted_spans[0]
 
 
-def _create_spans(verbs, doc):
-    patterns = [
-        [{"POS": "VERB"}, {"POS": "PART", "OP": "*"}, {"POS": "ADV", "OP": "*"}],
-        [
-            {"POS": "VERB"},
-            {"POS": "ADP", "OP": "*"},
-            {"POS": "DET", "OP": "*"},
-            {"POS": "AUX", "OP": "*"},
-            {"POS": "ADJ", "OP": "*"},
-            {"POS": "ADV", "OP": "*"},
-        ],
-    ]
+
+def create_spans(verbs, doc):
+    #Syntactic pattern looking for POS tags based on ReVerb's paper
+    patterns = [[{"POS": "VERB"}, {"POS": "PART", "OP": "*"}, {"POS": "ADV", "OP": "*"}],
+                [{"POS": "VERB"}, {"POS": "ADP", "OP": "*"}, {"POS": "DET", "OP": "*"},
+                 {"POS": "AUX", "OP": "*"},
+                 {"POS": "ADJ", "OP": "*"}, {"POS": "ADV", "OP": "*"}]]
 
     matcher = Matcher(nlp.vocab)
     matcher.add("Fluff", patterns)
     matches = matcher(doc)
     spans = []
     for match_id, start, end in matches:
-
         spans.append(doc.doc[start:end].text)
     res = []
     for verb in verbs:
         verbspans = [span for span in spans if verb in span]
-        span = _longest_span(verbspans)
+        # Since the matcher can catch multiple patterns with the same verb, we find the longest one
+        span = longest_span(verbspans)
         res.append(span)
 
     return res
 
 
-def _create_relation(span, span_index, entities):
+
+def create_relation(span, span_index, entities):
+    #Given a verb span, find the closest entity to the left and right of it.
 
     # Find left
     left_ent = None
@@ -72,22 +70,23 @@ def _create_relation(span, span_index, entities):
     relation = (span, left_ent, right_ent)
     return relation
 
-
-def _relation_extraction(doc):
+def relation_extraction(text):
+    doc = nlp(text)
     entities = doc.ents
-    verbs = _find_verbs(doc)
-    verbspans = _create_spans(verbs, doc)
+
+    #Filter out date entities for relation extraction, because it makes incoherent relations
+    entities = [e for e in entities if e.label_ not in ("DATE")]
+
+    verbs = find_verbs(doc)
+    verbspans = create_spans(verbs, doc)
     relations = []
     for span in verbspans:
         span_index = doc.text.index(span)
-        relation = _create_relation(span, span_index, entities)
+        (s, l,r) = create_relation(span, span_index, entities)
 
-        relations.append(relation)
+        if (l is not None and r is not None and str(l) != str(r)):
+            relations.append((s,l,r))
 
+    #print(relations)
+    #print(len(relations))
     return relations
-
-
-def extract_relations(input_text):
-    clusters = nlp(input_text)
-    doc = nlp(input_text)
-    return _relation_extraction(doc)
